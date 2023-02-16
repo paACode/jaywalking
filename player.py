00a7f0cea90083374c12ae0b838c2f45321a9e05
Pascal Ackermann <ackermann.pascal@gmail.con>
2023-02-16 07:57:01 +0100

    FEATURE: Removed unused class permanenbrush and cleaned up class player

--- player.py
+++ player.py
@@ -1,14 +1,9 @@
 import turtle
-import permanentbrush
 from PIL import Image
 
 from screen import SCREEN_HEIGHT_PX
 
-CHARACTER_WIDTH_PX = 40
-CHARACTER_HEIGHT_PX = 80
 DEFAULT_TURTLE_SIZE_PX = 20
-STEP = CHARACTER_HEIGHT_PX / 2
-STARTING_POSITION = (0, -SCREEN_HEIGHT_PX / 2 + CHARACTER_HEIGHT_PX / 2)
 DIRECTION_UP = 90
 
 
@@ -19,14 +14,16 @@ class Player(turtle.Turtle):
         self.penup()
         self.shape("granny.gif")
         self.width_px,  self.height_px = self.get_boundaries()
-        self.goto(STARTING_POSITION)
+        self.goto(0, -SCREEN_HEIGHT_PX / 2 + self.height_px / 2)
+        self.moving_distance = self.height_px/2
 
-    def get_boundaries(self):
+    @staticmethod
+    def get_boundaries():
         filepath = "granny.gif"
         img = Image.open(filepath)
         return img.width, img.height
 
     def move_up(self):
         self.setheading(DIRECTION_UP)
-        self.forward(STEP)
+        self.forward(self.moving_distance)
 
