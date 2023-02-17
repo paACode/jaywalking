0f3228167c36de7b3f43b20e40d45dca0831258e
Pascal Ackermann <ackermann.pascal@gmail.con>
2023-02-17 14:02:03 +0100

    FEATURE: Cars are not spawing with collisions anymore, basic game concept works

--- car.py
+++ car.py
@@ -4,20 +4,23 @@ from screen import SCREEN_WIDTH_PX, SCREEN_HEIGHT_PX
 
 DEFAULT_TURTLE_SIZE_PX = 20
 HEADING_WEST = 180
+CAR_WIDTH_PX = 40
+CAR_HEIGHT_PX = 20
+CAR_MOVING_DISTANCE_PX = 1
 
 
 class Car(turtle.Turtle):
     def __init__(self):
         super().__init__()
-        self.width_px = 40
-        self.height_px = 20
+        self.width_px = CAR_WIDTH_PX
+        self.height_px = CAR_HEIGHT_PX
         self.shape("square")
         self.penup()
         self.color(self.get_random_color())
         self.shapesize(self.height_px / DEFAULT_TURTLE_SIZE_PX, self.width_px / DEFAULT_TURTLE_SIZE_PX, 1)
         self.goto(self.get_random_start_coordinate())
         self.setheading(HEADING_WEST)
-        self.moving_distance = self.width_px/4
+        self.moving_distance = CAR_MOVING_DISTANCE_PX
 
     def move(self):
         self.forward(self.moving_distance)
@@ -28,7 +31,7 @@ class Car(turtle.Turtle):
 
 
     def get_random_start_coordinate(self):
-        x_start = SCREEN_WIDTH_PX / 2 - self.width_px / 2
+        x_start = SCREEN_WIDTH_PX / 2
         y_start = random.randrange(start=-SCREEN_HEIGHT_PX / 2 + DEFAULT_TURTLE_SIZE_PX / 2,
                                    stop=+SCREEN_HEIGHT_PX / 2 - DEFAULT_TURTLE_SIZE_PX / 2,
                                    step=self.height_px)
--- main.py
+++ main.py
@@ -4,18 +4,13 @@ import car
 import player
 import screen
 
+SCREEN_REFRESH_TIME_S = 0.02
+CAR_APPEND_TIME_S = SCREEN_REFRESH_TIME_S * car.CAR_WIDTH_PX / car.CAR_MOVING_DISTANCE_PX
+
 
 def initialize_controls():
     jaywalking_screen.listen()
     jaywalking_screen.onkey(jaywalker.move_up, "space")
-
-
-def timer_reached(seconds):
-    time_passed = timer_current_time - timer_start_time
-
-    if time_passed > seconds:
-        return True
-    return False
 
 
 def add_car():
@@ -27,11 +22,24 @@ def move_cars():
         every_car.move()
 
 
+def screen_refresh_needed(refresh_time_s=SCREEN_REFRESH_TIME_S):
+    if current_time - screen_refresh_start_time > refresh_time_s:
+        return True
+    return False
+
+
+def latest_car_ahead_by(distance_px):
+    latest_car_added = cars[len(cars) - 1]
+    print(screen.SCREEN_WIDTH_PX/2 - latest_car_added.xcor())
+    if screen.SCREEN_WIDTH_PX/2 - latest_car_added.xcor() > distance_px:
+        return True
+    return False
+
+
 def collision_with_car():
     for this_car in cars:
         delta_x = round(abs(this_car.xcor() - jaywalker.xcor()))
         delta_y = round(abs(this_car.ycor() - jaywalker.ycor()))
-        print(delta_y,delta_x)
         x_collision_threshold = jaywalker.width_px / 2 + this_car.width_px / 2
         y_collision_threshold = jaywalker.height_px / 2 + this_car.height_px / 2
         if delta_x < x_collision_threshold and delta_y < y_collision_threshold:
@@ -39,10 +47,10 @@ def collision_with_car():
     return False
 
 
-# def reached_other_side():
-#     if jaywalker.y_coordinate > screen.SCREEN_HEIGHT_PX / 2:
-#         return True
-#     return False
+def reached_other_side():
+    if jaywalker.ycor() > screen.SCREEN_HEIGHT_PX / 2:
+        return True
+    return False
 
 
 if __name__ == '__main__':
@@ -50,21 +58,22 @@ if __name__ == '__main__':
     jaywalker = player.Player()
     initialize_controls()
     cars = []
+    add_car()
     game_is_on = True
-    timer_start_time = time.time()
+    screen_refresh_start_time = time.time()
     while game_is_on:
-        timer_current_time = time.time()
-        if timer_reached(seconds=0.05):
-            timer_start_time = time.time()
-            add_car()
+        current_time = time.time()
+        if screen_refresh_needed(refresh_time_s=SCREEN_REFRESH_TIME_S):
+            screen_refresh_start_time = current_time
+            if latest_car_ahead_by(distance_px=car.CAR_WIDTH_PX):
+                add_car()
             move_cars()
+            jaywalking_screen.update()
+
         elif collision_with_car():
             game_is_on = False
-        # elif reached_other_side():
-        #     print("yippie")
-        #     game_is_on = False
-
-        # time.sleep(0.05)
-        jaywalking_screen.update()
+        elif reached_other_side():
+            print("yippie")
+            game_is_on = False
 
 jaywalking_screen.exitonclick()
